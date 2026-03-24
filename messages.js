153e85fb1b8a479732827da3eca5ea1d54a678ad
Kyle Booten <kbooten>
2026-03-24 07:27:20 +0100

    testing issue 2

---
+++ messages.js
@@ -0,0 +1,209 @@
+const items = [
+  {
+    id:"box-1",
+    message: `
+ABOUT
+[gap:4]
+
+ENSEMBLE
+[gap:6]
+
+PARK:
+[gap:4]
+[gap:4]
+ENSEMBLE 
+[gap:4]
+    PARK
+[gap:4]
+collects
+[gap:4]
+literary
+[gap:4]
+experiments
+[gap:3]
+in 
+[gap:4]
+human-computer co-writing. 
+[gap:8]
+Neither completely
+[gap:4]
+computer-generated 
+[gap:10]
+nor absent of 
+[gap:4]
+computational
+[gap:4]
+intervention, 
+the works explore
+[gap:4]
+how 
+[gap:4]
+machines may
+[gap:4]
+enter the literary 
+[gap:4]
+process iteratively,
+[gap:4]
+and how literary 
+[gap:4]
+artifacts may be
+[gap:4]
+changed by
+[gap:10]
+such iterations.
+[gap:15]
+Each piece is accompanied by
+[gap:15]
+both a 
+[gap:20]
+process note
+[gap:15]
+and 
+(in the
+[gap:20]
+"PLUS" edition, 
+[gap:20]
+print-only)
+[gap:40]
+a short 
+[gap:100]
+interview with the author.
+[gap:20] 
+[gap:20] 
+eds: Katy Ilonka Gero & Kyle Booten
+[gap:20] 
+[gap:20] 
+thanks U Sydney for support
+[gap:10] 
+  `
+  },
+
+  {
+    id:"box-2",
+    message: `
+ISSUE 1:
+[gap:6]
+Read
+[gap:6]
+Issue 1
+[gap:6]
+<a href="https://www.ensemblepark.com/issues/ensemble_park_1.pdf" target="_blank">HERE!</a>
+[gap:70]
+There is a print 
+[gap:50]
+issue
+[gap:70]
+(the "Plus" 
+version
+with interviews)
+but no 
+more
+[gap:30] 
+are available.
+[gap:20] 
+
+    `
+  },
+
+  {
+    id:"box-3",
+    message: `
+⭐⭐⭐⭐⭐⭐
+⭐ISSUE 2:⭐
+⭐⭐⭐⭐⭐⭐
+[gap:6]
+Read
+[gap:6]
+Issue 2
+[gap:6]
+<a href="https://www.ensemblepark.com/issues/ensemble_park_2.pdf">HERE!</a>
+[gap:70]
+There is a print issue
+[gap:70]
+(the "Plus" 
+with interviews)
+[gap:30] 
+write to
+[gap:50] 
+ensemblepark
+[gap:30] 
+@
+[gap:30] 
+gmail
+[gap:30] 
+.com
+[gap:20] 
+to inquire about collecting a copy.
+[gap:20] 
+
+    `
+  },
+
+  {
+    id:"box-5",
+    message: `
+PROCESS NOTE
+[gap:5]
+We ask that all 
+[gap:10]
+submissions
+[gap:20] 
+are accompanied by a short explanatory piece that documents
+[gap:40]
+and reflects
+[gap:45] 
+upon the co-authorship process, including relevant technical 
+[gap:10]
+details. 
+[gap:10]
+These descriptions of human+computer interactions 
+[gap:20] 
+should be specific enough to allow others
+[gap:10]
+to
+[gap:10]
+imitate
+[gap:40]
+them and imagine their own 
+[gap:60]
+writing process based on them.
+[gap:20] 
+    `
+  },
+
+  {
+    id:"box-4",
+    message: `
+SUBMISSIONS:
+[gap:30]
+FOR
+[gap:30]
+issue #3  
+[gap:30]
+we are 
+[gap:5]
+especiallyinterested 
+[gap:5]
+in:
+[gap:100]
+* repurposed quotidian linguistic technologies
+[gap:20] 
+* good Old-Fashioned AI, like schedulers, theorem provers, or ontologies
+[gap:30]
+* antagonistic interactions
+[gap:20] 
+* AI and not-AI
+[gap:20] 
+* writing that is "computational" in a non-trivial but only at one well-defined moment (e.g., *parsimonious AI*)
+[gap:20]
+* custom language models / small language models
+[gap:20] 
+* world building
+[gap:40]
+* unusual interaction paradigms (computer as forest, geode, sticker pack)
+[gap:10]
+* experiments with scale (e.g. agent swarms).
+[gap:10]
+`
+  }
+];
