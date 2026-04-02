378b4175c76386e445e780694468ce387e6ce077
Kyle Booten <kbooten>
2026-04-01 22:20:46 -0400

    new page...simpler, green squares, wide open walk

---
+++ the-data.js
@@ -0,0 +1,314 @@
+const STORY_TEXT = `
+This is the website
+<break3>
+of
+<r>
+<l>
+<r>
+
+<break2>
+ENSEMBLE PARK: A JOURNAL
+<break1>
+OF HUMAN+COMPUTER
+<break2>
+WRITING.
+<r>
+<r>
+<break5>
+<l>
+<break10>
+<r>
+<break20>
+<l>
+<l>
+<break5>
+<l>
+<l>
+<break10>
+The newest issue is Issue #2. 
+<break3>
+You can find it <a href="https://www.ensemblepark.com/issues/ensemble_park_2.pdf" target="_blank">HERE</a>.
+<break10>
+<r>
+<r>
+<break30>
+<r>
+<r>
+<r>
+<break5>
+<l>
+There is also a print
+<l>
+version (the "PLUS" version) 
+<l>
+with additional stuff 
+<break3>
+<r>
+<r>
+<l>
+(interviews with the
+<r>
+authors).
+<break16>
+<l>
+<l>
+<l>
+<r>
+<break2>
+Email us
+<break2>
+(ensembleparkjournal
+<break5>
+at gmail) to inquire about
+<break3>
+<r>
+collecting a copy.
+<r>
+<break20>
+<l>
+<r>
+<break20>
+<r>
+<r>
+<break30>
+<r>
+<r>
+<r>
+<break5>
+<l>
+Issue #1 can be found 
+<break3>
+<a href="https://www.ensemblepark.com/issues/ensemble_park_1.pdf" target="_blank">HERE</a>.
+<break10>
+<r>
+<r>
+<l>
+<break10>
+<r>
+<break3>
+<r>
+<break50>
+<r>
+<r>
+<l>
+<break5>
+<l>
+<r>
+<break60>
+<r>
+<r>
+<r>
+<break5>
+<l>
+Neither completely
+<r>
+<break10>
+<r>
+<break3>
+computer-generated
+<break4>
+nor absent of
+<l>
+computational intervention,
+<break8>
+the works explore
+<r>
+how machines may enter
+<break3>
+a writing process
+<l>
+iteratively,
+<break10>
+<break5>
+<r>
+<break10>
+and how textual artifacts
+<r>
+may be changed
+<break3>
+by such
+<l>
+iterations.
+<break28>
+<l>
+<break28>
+<r>
+<break20>
+<r>
+<r>
+<l>
+Each piece
+<break3>
+is accompanied by
+<l>
+a Process Note
+<break8>
+so that readers
+<break10>
+may learn from, borrow, and adapt
+<break10>
+tactics.
+<break3>
+<r>
+<break10>
+<l>
+<break10>
+<l>
+<r>
+<break20>
+<l>
+<l>
+<break2>
+<l>
+<break10>
+<r>
+<break3>
+<r>
+<break20>
+<r>
+<r>
+<l>
+<break5>
+<l>
+<r>
+<break30>
+<r>
+<r>
+<break5>
+<l>
+SUBMISSIONS
+<l>
+<r>
+For issue #3
+<r>
+we are especially
+<break3>
+interested in:
+<break10>
+<l>
+repurposed quotidian
+linguistic technologies
+<break10>
+<l>
+good Old-Fashioned AI
+<r>
+like schedulers,
+<break2>
+theorem provers,
+<break2>
+or ontologies
+<break14>
+antagonistic
+<l>
+interactions
+<break22>
+AI
+<r>
+and not-AI
+<break12>
+<break12>
+anti-slop studies
+<break12>
+<r>
+writing that is
+<l>
+"computational"
+<break2>
+in a non-trivial way
+<r>
+but only at
+<break2>
+one well-defined moment
+<break2>
+("parsimonious AI")
+<break16>
+<l>
+custom language models
+<l>
+small language models
+<l>
+<l>
+<break14>
+world building
+<r>
+<break14>
+unusual interaction paradigms
+<l>
+<break10>
+computer as forest
+<break2>
+computer as geode
+<break2>
+computer as sticker pack
+<break18>
+<r>
+<break3>
+experiments
+with scale
+<break3>
+<l>
+<r>
+(e.g. agent swarms).
+<break24>
+<r>
+<r>
+<r>
+<r>
+<l>
+Email submissions to
+<break2>
+ensembleparkjournal
+<break5>
+at gmail.
+<break20>
+<l>
+<break50>
+<r>
+<l>
+<break20>
+<l>
+<l>
+<break20>
+<r>
+<l>
+<break20>
+<r>
+<break40>
+<l>
+<break30>
+<break10>
+<r>
+<l>
+<l>
+<l>
+<break60>
+<l>
+<l>
+ENSEMBLE PARK is
+<break30>
+Katy Ilonka Gero
+<l>
+&
+<break2>
+Kyle Booten
+<r>
+<break10>
+<r>
+<break10>
+<r>
+<break10>
+<r>
+<break60>
+Thanks
+<break3>
+U Sydney
+<l>
+for support.
+<l>
+<l>
+<break20>
+<l>
+<r>
+<l>
+<r>
+`;
